a3432bc8cc717307d8cbe3922979f86413606847
Chima <thechimaobii@gmail.com>
2023-02-17 15:32:13 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
