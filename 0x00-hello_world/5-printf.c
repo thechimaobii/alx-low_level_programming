5a7fdedc71b09c4ca78273f3ddda688e785f4336
Chima Ifeji <thechimaobii@gmail.com>
2023-02-16 18:53:56 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the central part of the code
+ * Return: gives back 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
