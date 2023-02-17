1fb442418dded88b1d61c7b016c3f6a302aa69cf
Chima Ifeji <thechimaobii@gmail.com>
2023-02-16 20:30:37 -0800

    new sizez

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - central part of code and prints sizes
+ * main - central part of code and prints size of datatypes
  *
  * Return: gives back 0
  */
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
