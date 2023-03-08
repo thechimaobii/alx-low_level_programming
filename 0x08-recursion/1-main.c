c068cc657411480e2cdfd6654411c6d2e1c639a3
Chima <thechimaobii@gmail.com>
2023-03-08 14:45:34 -0800

    print reverse string

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,14 +1,10 @@
-#include "main.h"
 /**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: The string to be printed.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void _print_rev_recursion(char *s)
+int main(void)
 {
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+    _print_rev_recursion("\nColton Walker");
+    return (0);
 }
-
